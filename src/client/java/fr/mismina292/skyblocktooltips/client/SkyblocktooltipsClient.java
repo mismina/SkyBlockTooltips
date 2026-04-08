c09052e6e6d0fc28f2503e5e9dbb84d9c974bc42
mismina <161147756+mismina@users.noreply.github.com>
2026-04-08 14:44:50 +0200

    Initial commit

---
+++ src/client/java/fr/mismina292/skyblocktooltips/client/SkyblocktooltipsClient.java
@@ -0,0 +1,10 @@
+package fr.mismina292.skyblocktooltips.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkyblocktooltipsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/fr/mismina292/skyblocktooltips/Skyblocktooltips.java
@@ -0,0 +1,10 @@
+package fr.mismina292.skyblocktooltips;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Skyblocktooltips implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
